d768ac0698105034942ec310cf6f496d37f29f3d
AyelmerCorzoB <corzobarbosaa@gmail.com>
2025-02-21 16:12:19 -0500

    fix: :bug: Correcion del warning en el scanner
    By @AyelmerCorzoB

--- programa/src/main/java/com/gestion_de_estudiantes/Main.java
+++ programa/src/main/java/com/gestion_de_estudiantes/Main.java
@@ -9,32 +9,33 @@ public class Main {
     public static void main(String[] args) {
             LinkedList<Estudiante> ListaEstudiantes = new LinkedList<>();
             int cantidadEstudiantes;
-            Scanner sc = new Scanner(System.in);
-            do {
-                System.out.print("Ingresa la cantidad de estudiantes que deseas registrar (Max 30): ");
-                cantidadEstudiantes = sc.nextInt();
-            } while (cantidadEstudiantes >= 30);
-            System.out.println("La cantidad de estudiantes es: " + cantidadEstudiantes);
-    
-            sc.nextLine();
-    
-            for (int i = 0; i < cantidadEstudiantes; i++) {
-                System.out.print("Ingresa el nombre del estudiante " + (i + 1) + ": ");
-                String nombreEstudiante = sc.nextLine();
-                ListaEstudiantes.add(new Estudiante(nombreEstudiante));
-            }
-    
-            ListaEstudiantes.sort(Comparator.comparing(Estudiante::getName));
-            System.out.println("Lista de estudiantes ordenados de (A-Z)");
-            Funciones.mostrarListaEstudiantes(ListaEstudiantes);
-    
-            System.out.print("Ingresa el nombre del estudiante que deseas buscar: ");
-            String nombreABuscar = sc.nextLine();
-            boolean existe = Funciones.buscarEstudiantes(ListaEstudiantes, nombreABuscar);
-            if (existe) {
-                System.out.println("El estudiante " + nombreABuscar + " está en la lista.");
-            } else {
-                System.out.println("El estudiante " + nombreABuscar + " no está en la lista.");
+            try (Scanner sc = new Scanner(System.in)) {
+                do {
+                    System.out.print("Ingresa la cantidad de estudiantes que deseas registrar (Max 30): ");
+                    cantidadEstudiantes = sc.nextInt();
+                } while (cantidadEstudiantes >= 30);
+                System.out.println("La cantidad de estudiantes es: " + cantidadEstudiantes);
+   
+                sc.nextLine();
+   
+                for (int i = 0; i < cantidadEstudiantes; i++) {
+                    System.out.print("Ingresa el nombre del estudiante " + (i + 1) + ": ");
+                    String nombreEstudiante = sc.nextLine();
+                    ListaEstudiantes.add(new Estudiante(nombreEstudiante));
+                }
+   
+                ListaEstudiantes.sort(Comparator.comparing(Estudiante::getName));
+                System.out.println("Lista de estudiantes ordenados de (A-Z)");
+                Funciones.mostrarListaEstudiantes(ListaEstudiantes);
+   
+                System.out.print("Ingresa el nombre del estudiante que deseas buscar: ");
+                String nombreABuscar = sc.nextLine();
+                boolean existe = Funciones.buscarEstudiantes(ListaEstudiantes, nombreABuscar);
+                if (existe) {
+                    System.out.println("El estudiante " + nombreABuscar + " está en la lista.");
+                } else {
+                    System.out.println("El estudiante " + nombreABuscar + " no está en la lista.");
+                }
             }
         }
     
